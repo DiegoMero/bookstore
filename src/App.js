38296e41bcbcdda51412a501ca46317f20651a4d
Diego Mero <diegomero_1999@hotmail.com>
2022-08-19 14:26:20 -0500

    Fix css linter errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
 import Header from './components/Header';
 import BooksList from './components/BookList';
 import Categories from './components/Categories';
