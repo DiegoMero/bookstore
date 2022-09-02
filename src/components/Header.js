aefaebd80a7d42937ffc886739cd43334a300057
Diego Mero <diegomero_1999@hotmail.com>
2022-09-02 00:35:42 -0500

    Add header css style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => (
-  <header>
+  <header className="header">
     <h1>Bookstore CMS</h1>
     <div>
-      <Link to="/">BOOKS</Link>
-      <Link to="/Categories">CATEGORIES</Link>
+      <Link to="/" className="book-header">BOOKS</Link>
+      <Link to="/Categories" className="categories-header">CATEGORIES</Link>
+    </div>
+    <div className="user-border">
+      <i className="fa-solid fa-user" />
     </div>
   </header>
 );
