463891796545a0b83af93a4c3df158a2b8c199d5
Diego Mero <diegomero_1999@hotmail.com>
2022-09-02 00:37:48 -0500

    Delete unnecessary comments

--- src/components/APIurl.js
+++ src/components/APIurl.js
@@ -1,4 +1,3 @@
-// export const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/cKFfxRYPps1kHshp1Jyn';
 const appID = 'cKFfxRYPps1kHshp1Jyn';
 const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}`;
 
